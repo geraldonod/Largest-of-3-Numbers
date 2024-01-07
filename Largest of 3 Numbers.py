84df65c0b0127de1d567e602f53304e618fb6f87
Gerald <geraldonod@gmail.com>
2024-01-07 18:31:37 +0800

    added if else statements

--- Largest of 3 Numbers.py
+++ Largest of 3 Numbers.py
@@ -5,5 +5,11 @@ number_2 = float(input("Enter the second number: "))
 number_3 = float(input("Enter the third number: "))
 
 #Compare number values with if else statements
+if number_1 >= number_2 and number_1 >= number_3:
+    largest = number_1
+elif number_2 >= number_1 and number_2 >= number_3:
+    largest = number_2
+else:
+    largest = number_3
 
 #Print result
