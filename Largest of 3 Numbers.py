364485208f57b190f624643cd863a9bfc37138fe
Gerald <geraldonod@gmail.com>
2024-01-07 18:00:19 +0800

    added ask user input

--- Largest of 3 Numbers.py
+++ Largest of 3 Numbers.py
@@ -1,5 +1,8 @@
 #pseudocode
 #Get input from user
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
+number_3 = float(input("Enter the third number: "))
 
 #Compare number values with if else statements
 
