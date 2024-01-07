ba1e9b846ad6a46189e0cbfc977d5081b346fa93
Gerald <geraldonod@gmail.com>
2024-01-07 19:00:31 +0800

    added try except error message

--- Largest of 3 Numbers.py
+++ Largest of 3 Numbers.py
@@ -3,18 +3,23 @@
 # Greeting
 print("Hi there! Please input three numbers and I will identify which one has the largest value.")
 
-#Get input from user
-number_1 = int(input("Enter the first number: "))
-number_2 = int(input("Enter the second number: "))
-number_3 = int(input("Enter the third number: "))
+try:
+    #Get input from user
+    number_1 = int(input("Enter the first number: "))
+    number_2 = int(input("Enter the second number: "))
+    number_3 = int(input("Enter the third number: "))
 
-#Compare number values with if else statements
-if number_1 >= number_2 and number_1 >= number_3:
-    largest = number_1
-elif number_2 >= number_1 and number_2 >= number_3:
-    largest = number_2
-else:
-    largest = number_3
+    #Compare number values with if else statements
+    if number_1 >= number_2 and number_1 >= number_3:
+     largest = number_1
+    elif number_2 >= number_1 and number_2 >= number_3:
+     largest = number_2
+    else:
+     largest = number_3
 
-#Print result
-print("Hey! The largest number you inputted is:", largest)
+    #Print result
+    print("Hey! The largest number you inputted is:", largest)
+
+except: 
+    #Error message
+        print("Oops! Please enter valid numeric values.")
